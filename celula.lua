fd15899abc731a176445804367a1bd5bf4926f68
Mario Nakazato <marionakazat@gmail.com>
2023-05-01 01:17:51 -0300

    v 1.0

--- celula.lua
+++ celula.lua
@@ -1,126 +1,122 @@
-local celula = {}
-
-celula.numeroMinas = 0
-celula.totalMinas = 10
-
-function celula.nova(i, j, l)
-
-  local objeto = {}
+local function nova(i, j, x, y, l)
   
+  objeto = {}
   objeto.i = i
   objeto.j = j
-  objeto.x = i *l
-  objeto.y = j *l
   objeto.l = l
+  objeto.x = (i +x) *l
+  objeto.y = (j +y) *l
   objeto.visivel = false
-  objeto.contagem = 0
-  objeto.fonte = love.graphics.newFont(objeto.l/2)
-
-  if love.math.random() < 0.1 and celula.numeroMinas < celula.totalMinas then
+  objeto.mina = false
+  objeto.bandeira = false
+  objeto.contagemMina = 0
+  objeto.fonte = lgrafico.newFont(l /2)
   
-    objeto.mina = true
-    celula.numeroMinas = celula.numeroMinas +1
-  
-  else
-
-    objeto.mina = false
-  
-  end
-
   function objeto:desenhar()
     
-    love.graphics.setColor(converterCor(0, 0, 0))
-    love.graphics.rectangle("line", self.x, self.y, self.l, self.l)
-  
+    lgrafico.setColor(corByte(0, 0, 0))
+    lgrafico.rectangle("line", self.x, self.y, self.l, self.l)
+    lgrafico.setColor(corByte(95, 95, 95))
+    lgrafico.rectangle("fill", self.x +1, self.y +1, self.l -1, self.l -1)
     if self.visivel then
+      lgrafico.setColor(corByte(159, 159, 159))
+      lgrafico.rectangle("fill", self.x +1, self.y +1, self.l -1, self.l -1)
+      lgrafico.setColor(corByte(0, 0, 0))
       if self.mina then
-        
-        love.graphics.setColor(converterCor(163, 163, 163))
-        love.graphics.rectangle("fill", self.x +2, self.y +2, self.l -2, self.l -2)
-        love.graphics.setColor(converterCor(107, 107, 107))
-        love.graphics.circle("fill", self.x +self.l*0.5, self.y +self.l*0.5, self.l*0.25)
-        love.graphics.setColor(converterCor(118, 118, 118))
-        love.graphics.circle("fill", self.x +self.l*0.5, self.y +self.l*0.5, self.l*0.1)
-      
+        lgrafico.circle("line", self.x +self.l *0.5, self.y +self.l *0.5, self.l *0.25)
       else
-        
-        love.graphics.setColor(converterCor(163, 163, 163))
-        love.graphics.rectangle("fill", self.x +2, self.y +2, self.l -2, self.l -2)
-        love.graphics.setColor(converterCor(0, 0, 0))
-        
-        if self.contagem > 0 then
-          
-          love.graphics.setFont(self.fonte)
-          love.graphics.print(self.contagem, self.x +self.l*0.4, self.y +self.l*0.4)
-        
+        if self.contagemMina > 0 then
+          lgrafico.setFont(self.fonte)
+          lgrafico.print(self.contagemMina, self.x +self.l*0.35, self.y +self.l*0.25)
         end
       end
     end
+    lgrafico.setColor(corByte(255, 69, 0))
+    if self.bandeira then
+      lgrafico.circle("fill", self.x +self.l *0.5, self.y +self.l *0.5, self.l *0.125)
+    elseif self.bandeira == nil then
+      lgrafico.line(self.x, self.y, self.x+ self.l, self.y+ self.l)
+      lgrafico.line(self.x +self.l, self.y, self.x, self.y+ self.l)
+    end
+    
   end
   
-  function objeto:selecao(x, y)
-    
+  function objeto:selecionar(x, y)
+  
     return (x > self.x and x < self.x +self.l and y > self.y and y < self.y +self.l)
+  
+  end
+
+  function objeto:mostrar()
+    
+    abriu = 0
+    if not self.visivel and not self.bandeira then
+      self.visivel = true
+      if not self.mina and self.contagemMina == 0 then
+        abriu = abriu +self:mostrarVisinho()
+      end
+      abriu = abriu +1
+    end
+    
+    return abriu
     
   end
   
   function objeto:contarMina()
     
     if self.mina then
-      
-      return -1
-      
+      return
     end
     
     total = 0
     
-    for i = 0, 2 do
-      for j = 0, 2 do
+    for i = -1, 1 do
+      for j = -1, 1 do
         if grade[self.i +i] then
           if grade[self.i +i][self.j +j] then
             if grade[self.i +i][self.j +j].mina then
-          
+              
               total = total +1
-          
+              
             end
           end
         end
       end
     end
     
-    self.contagem = total
+    self.contagemMina = total
     
-  end
-  
-  function objeto:mostrar()
-      
-      self.visivel = true
-      if self.contagem == 0 and not self.mina then
-        
-        self:mostrarVisinho()
-      
-      end
   end
   
   function objeto:mostrarVisinho()
     
-    for i = 0, 2 do
-      for j = 0, 2 do
+    abriu = 0
+    for i = -1, 1 do
+      for j = -1, 1 do
         if grade[self.i +i] then
           if grade[self.i +i][self.j +j] then
             if not grade[self.i +i][self.j +j].visivel and not grade[self.i +i][self.j +j].mina then
-          
-              grade[self.i +i][self.j +j]:mostrar()
-          
+              
+              abriu = abriu +grade[self.i +i][self.j +j]:mostrar()
+              
             end
           end
         end
       end
     end
+    
+    return abriu
+    
   end
   
   return objeto
-
+  
 end
+  
+celula = {
+  
+  nova = nova,
+  
+}
 
 return celula
--- main.lua
+++ main.lua
@@ -1,97 +1,366 @@
-local celula = require("celula")
+--celula = require "celula"
+require "celula"
 
-local larguraTela = 500
-local alturaTela = 500
-local tamanhoCelula = 25
-grade = {}
-local jogando = true
-local totalCelulas = larguraTela / tamanhoCelula * alturaTela / tamanhoCelula
-local totalMinas = math.floor(totalCelulas / 6)
-local numeroMinas = 0
-local fontePontuacao
-local tempoInicio = 0
-local pontuacao = 0
-
-function love.load()
-  love.window.setTitle("Campo Minado")
-  love.window.setMode(larguraTela, alturaTela)
-  love.graphics.setBackgroundColor(converterCor(255, 255, 255))
-
-  fontePontuacao = love.graphics.newFont(30)
-
-  for i = 0, larguraTela / tamanhoCelula - 1 do
-    grade[i] = {}
-
-    for j = 0, alturaTela / tamanhoCelula - 1 do
-      grade[i][j] = celula.nova(i, j, tamanhoCelula)
+function love.load(arg)
+  
+  if arg[#arg] == "-debug" then require("mobdebug").start() end -- ZeroBrane Studio para Debug
+  
+  lgrafico = love.graphics
+  lmath = love.math
+  
+  corByte = lmath.colorFromBytes
+  
+  update = false
+  draw = true
+  mousepressed = true
+  
+  ciclo = 0
+  tick = 0
+  ticks = 0
+  
+  segundo = 0
+  minuto = 0
+  hora = 0
+  
+  lado = 32--80
+  linha = 16--28--10--28
+  coluna = 16--58--10--58
+  
+  love.window.updateMode(lado *coluna +16, lado *linha +16 +64 +8)
+  ct, lt = lgrafico.getDimensions()
+  
+  level = 1
+  txt = "Level: " ..level
+  
+  timer = {}
+  timer.c = 128
+  timer.l = 64
+  timer.x = math.floor((ct -timer.c) /2)
+  timer.y = 8
+  timer.txt = "0000"
+  timer.fonte = lgrafico.newFont(timer.l /2)
+  function timer:contar(seg)
+    seg = seg%10000
+    if seg < 10 then
+      self.txt = "000" ..seg
+    elseif seg >= 10 and seg < 100 then
+      self.txt = "00" ..seg
+    elseif seg >= 100 and seg < 1000 then
+      self.txt = "0" ..seg
+    else
+      self.txt = seg
     end
   end
-
-  while numeroMinas < totalMinas do
-    local i = love.math.random(0, larguraTela / tamanhoCelula - 1)
-    local j = love.math.random(0, alturaTela / tamanhoCelula - 1)
-
+  
+  x, y = (ct -lado *coluna) /lado /2 -1, (lt -8 -lado *linha) /lado -1
+  
+  grade = {}
+  
+  for i = 1, coluna do
+    grade[i] = {}
+    for j = 1, linha do
+      grade[i][j] = celula.nova(i, j, x, y, lado)
+    end  
+  end
+  
+  mina = 32
+  bandeiras = mina
+  while mina > 0 do
+    i, j = lmath.random(coluna), lmath.random(linha)
     if not grade[i][j].mina then
       grade[i][j].mina = true
-      numeroMinas = numeroMinas + 1
+      mina = mina -1
     end
   end
+  mina = bandeiras
+  
+  for i = 1, coluna do
+    for j = 1, linha do
+      grade[i][j]:contarMina()
+    end  
+  end
+  
+  abriu = 0
+  
 end
 
 function love.update(dt)
-  if jogando then
-    pontuacao = math.floor(love.timer.getTime() - tempoInicio)
+  
+  if not update then
+    return
   end
+  
+  timer:contar(math.floor(ticks))
+  
+  ciclo = ciclo +1
+  tick = dt
+  ticks = ticks +tick
+  segundo = math.floor(ticks) %60
+  minuto = math.floor(ticks /60) %60
+  hora = math.floor(ticks /3600)
+  
 end
 
 function love.draw()
-  for i = 0, larguraTela / tamanhoCelula - 1 do
-    for j = 0, alturaTela / tamanhoCelula - 1 do
-      grade[i][j]:desenhar()
-    end
-  end
-
-  love.graphics.setFont(fontePontuacao)
-  love.graphics.print("Pontuação: " .. pontuacao, 10, 10)
-end
-
-function love.mousepressed(x, y, button)
-  if button ~= 1 or not jogando then
+  
+  if not draw then
     return
   end
+  
+  lgrafico.setBackgroundColor(corByte(0, 108, 0))
+  lgrafico.setLineStyle("smooth")
+  lgrafico.setLineWidth(4)
+  
+  lgrafico.setColor(corByte(0, 0, 0))
+  lgrafico.rectangle("line", (ct -lado *coluna) /2, 8, lado *coluna, 64)
+  
+  lgrafico.setFont(timer.fonte)
+  lgrafico.setColor(corByte(255, 69, 0))
+  lgrafico.circle("fill", timer.x -100, timer.y +32, lado *0.125)
+  lgrafico.setColor(corByte(0, 0, 0))
+  lgrafico.print(bandeiras, timer.x -72, timer.y +timer.l *0.2)
+  
+  lgrafico.rectangle("line", timer.x, timer.y, timer.c, timer.l)
+  lgrafico.print(timer.txt, timer.x +timer.c *0.2, timer.y +timer.l *0.2)
+  
+  lgrafico.print(txt, timer.x +140, timer.y +timer.l *0.2)
+  
+  for i = 1, coluna do
+    for j = 1, linha do
+      grade[i][j]:desenhar()
+    end  
+  end
+  
+end
 
-  for i = 0, larguraTela / tamanhoCelula - 1 do
-    for j = 0, alturaTela / tamanhoCelula - 1 do
-      if grade[i][j]:selecao(x, y) then
-        grade[i][j]:mostrar()
-
-        if grade[i][j].mina then
-          jogando = false
-          tempoInicio = 0
-        elseif verificarVitoria() then
-          jogando = false
+function love.keypressed(tecla, cod, repeticao)
+  
+  if tecla == "kp+" and level < 10 then
+    level = level +1
+    txt = "Level: " ..level
+  elseif tecla == "kp-" and level > 1 then
+    level = level -1
+    txt = "Level: " ..level
+  end
+  
+  if tecla == "f5" then
+    love.load({})
+  elseif tecla == "f6" then
+    update = false
+    mousepressed = false
+    for i = 1, coluna do
+      for j = 1, linha do
+        
+        if not grade[i][j].mina and grade[i][j].bandeira then
+          grade[i][j].bandeira = nil
+        else
+          grade[i][j].visivel = true
         end
+        
       end
     end
   end
-
-  if not tempoInicio then
-    tempoInicio = love.timer.getTime()
-  end
+  
 end
 
-function verificarVitoria()
-  for i = 0, larguraTela / tamanhoCelula - 1 do
-    for j = 0, alturaTela / tamanhoCelula - 1 do
-      if not grade[i][j].visivel and not grade[i][j].mina then
-        return false
+function love.keyreleased(tecla, cod)
+  
+  
+  
+end
+
+function love.mousepressed(x, y, botao, toque, repeticao)
+  
+  if not mousepressed then
+    return
+  end
+  
+  for i = 1, coluna do
+    for j = 1, linha do
+      
+      if grade[i][j]:selecionar(x, y) then
+        if botao == 1 and not grade[i][j].bandeira then
+          update = true
+          if grade[i][j].mina then
+            
+            mousepressed = false
+            txt = "Perdeu!"
+            for i = 1, coluna do
+              for j = 1, linha do
+                
+                if grade[i][j].mina then
+                  update = false
+                   abriu = abriu +grade[i][j]:mostrar()
+                else
+                  if grade[i][j].bandeira then
+                    grade[i][j].bandeira = nil
+                  end
+                end
+                
+              end  
+            end
+            
+          else
+            abriu = abriu +grade[i][j]:mostrar()
+          end
+        elseif botao == 2 then
+          if not grade[i][j].visivel then
+            grade[i][j].bandeira = not grade[i][j].bandeira
+            if grade[i][j].bandeira then
+              bandeiras = bandeiras -1
+            elseif not grade[i][j].bandeira and bandeiras < mina then
+              bandeiras = bandeiras +1
+            end
+          end
+        end
+        break
       end
+      
     end
   end
-
-  return true
+  
+  if abriu == coluna *linha -mina then
+    update = false
+    mousepressed = false
+    txt = "Ganhou!"
+    
+  end
+  
 end
 
-function converterCor(r, g, b)
-  return r / 255, g / 255, b / 255
+function love.mousereleased(x, y, botao, toque, repeticao)
+  
+  
+  
 end
+
+function love.mousemoved(x, y, dx, dy, toque)
+  
+  
+  
+end
+
+function love.wheelmoved(x, y)
+  
+  
+  
+end
+
+function love.mousefocus(foco)
+  
+  
+  
+end
+
+function love.resize(c, l)
+  
+  
+  
+end
+
+function love.focus(foco)
+  
+  
+  
+end
+
+function love.quit()
+  
+  
+  
+end
+--[[
+function inicioContato(a, b, contato) 
+  
+  
+  
+end
+
+function fimContato(a, b, contato)
+  
+  
+  
+end
+
+function preContato(a, b, contato)
+  
+  
+  
+end
+
+--function posContato(a, b, contato, normalImpulso, tangenteImpulso, normalImpulso1, tangenteImpulso1)
+function posContato(a, b, contato, normalImpulso, tangenteImpulso)
+  
+  
+  
+end
+
+function love.touchpressed(id, x, y, dx, dy, pressao)
+  
+  
+  
+end
+
+function love.touchreleased(id, x, y, dx, dy, pressao)
+  
+  
+  
+end
+
+function love.touchmoved(id, x, y, dx, dy, pressao)
+  
+  
+  
+end
+
+function love.displayrotated(indice, orientacao)
+  
+  
+  
+end
+
+function love.textedited(texto, inicio, tamanho)
+  
+  
+  
+end
+
+function love.textinput(texto)
+  
+  
+  
+end
+
+function love.directorydropped(caminho)
+  
+  
+  
+end
+
+function love.filedropped(arquivo)
+  
+  
+  
+end
+
+function love.errorhandler(erro)
+  
+  
+  
+end
+
+function love.lowmemory()
+  
+  
+  
+end
+
+function love.threaderror(thread, erro)
+  
+  
+  
+end
+
+function love.visible(visivel)-- Esta funcao CallBack não funciona, utilize visivel = love.window.isMinimized()
+end
+--]]
